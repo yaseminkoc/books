5bf063a784d1a16aad0efc56912ee429346b8abd
Yasemin Koc <yaseminkocc37@gmail.com>
2022-08-23 02:52:55 +0300

    first commit

---
+++ src/components/Book.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Book extends React.Component {
+    render() {
+        return (
+          <div>{this.props.book.title}</div> 
+    )
+    }
+
+}
+export default Book;
