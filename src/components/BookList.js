fc950ad62eb97fb7e03c201a3247b0c30f09d074
Yasemin Koc <yaseminkocc37@gmail.com>
2022-08-23 21:32:21 +0300

    Default Value Context added.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,31 @@
 import React from "react";
-import Book from "./Book"
+import Book from "./Book";
+import './BookList.css';
+import  BookContext from "../contexts/BookContext";
+
 class BookList extends React.Component {
+    static contextType = BookContext;
+
     render() {
-        const bookList = this.props.books.map((book, i) => {
-           return <Book book={book} key={i} />
+        const bookList = this.context.map((book, i) => {
+            return <Book book={book} key={i} />
         });
 
         return (
-            <div>{bookList}</div>
+            <section className="page-section bg-light" id="portfolio">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">KOC BookShop</h2>
+                        <h3 className="section-subheading text-muted">This the best until we will make the better one.</h3>
+                    </div>
+                    <div className="row">
+
+                        {bookList}
+
+                    </div>
+                </div>
+            </section>
+
         )
     }
 }
