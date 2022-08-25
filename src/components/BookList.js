abde51b42a7405132c8e81fd7b299593082093e1
Yasemin Koc <yaseminkocc37@gmail.com>
2022-08-26 00:13:21 +0300

    change theme button added

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -7,38 +7,33 @@ class BookList extends React.Component {
     render() {
         return (
             <ThemeContext.Consumer>{(contextTheme) => (
-            
-           
-            <BookContext.Consumer>
-                {contextBook => {  
-                const {isDarkTheme, dark, light} = contextTheme; 
-                const {books} = contextBook; 
-                const theme = isDarkTheme ? dark : light;
+                <BookContext.Consumer>
+                    {contextBook => {
+                        const { changeTheme, isDarkTheme, dark, light } = contextTheme;
+                        const { books } = contextBook;
+                        const theme = isDarkTheme ? dark : light;
 
-              
-                    return (
-                        <section className="page-section" style= {{background : theme.bg, color:theme.txt}} id="portfolio">
-                            <div className="container">
-                                <div className="text-center">
-                                    <h2 className="section-heading text-uppercase">KOC BookShop</h2>
-                                    <h3 className="section-subheading text-muted">This the best until we will make the better one.</h3>
+
+                        return (
+                            <section className="page-section" style={{ background: theme.bg, color: theme.txt }} id="portfolio">
+                                <div className="container">
+                                    <div className="text-center">
+                                        <h2 className="section-heading text-uppercase">KOC BookShop</h2>
+                                        <h3 className="section-subheading text-muted">This the best until we will make the better one.</h3>
+                                        <button type="button" className="btn btn-sm btn-info" style={{marginTop:"-80px"}} onClick={changeTheme}>Change Theme</button>
+                                    </div>
+                                    <div className="row">
+
+                                        {books.map((book, i) => {
+                                            return <Book book={book} key={i} />
+                                        })}
+
+                                    </div>
                                 </div>
-                                <div className="row">
-
-                                    {books.map((book, i) => {
-                                        return <Book book={book} key={i} />
-                                    })}
-
-                                </div>
-                            </div>
-                        </section>
-                    )
-                }}
-            </BookContext.Consumer>)}
-
-
-                
-
+                            </section>
+                        )
+                    }}
+                </BookContext.Consumer>)}
             </ThemeContext.Consumer>
 
         )
